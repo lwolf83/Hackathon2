e3e8489039c71846cc5d12761d99707c462a9ccf
Elisa ROMAIN <romain.elisa@yahoo.fr>
2020-04-16 10:59:40 +0200

    design FightArena

--- View/FightArena.xaml.cs
+++ View/FightArena.xaml.cs
@@ -29,7 +29,13 @@ namespace Hackathon2
             Joueur1.Init();
             Joueur2 = ApiRequest.GetCharacter(97);
             Joueur2.Init();
-
+            Player1_Image.Source = new BitmapImage(new Uri(Joueur1.image.Url));
+            Player1_Name.Content = Joueur1.name;
+            Player2_Image.Source = new BitmapImage(new Uri(Joueur2.image.Url));
+            Player2_Name.Content = Joueur2.name;
+            PV1.Content = Joueur1.PV;
+            PV2.Content = Joueur2.PV;
+            AQuiLeTour.Content = Joueur1.name + " vs " + Joueur2.name;
             Fight = new Fight(Joueur1, Joueur2);
 
         }
@@ -42,6 +48,8 @@ namespace Hackathon2
         private void J1_AttPhys_Btn(object sender, RoutedEventArgs e)
         {
             PlayerAttack(Joueur1, Joueur2, "Physical Attack");
+            PV2.Content = Joueur2.PV;
+
         }
 
 
@@ -49,19 +57,21 @@ namespace Hackathon2
         private void J1_AttInt_Btn(object sender, RoutedEventArgs e)
         {
             PlayerAttack(Joueur1, Joueur2, "Intellectual Attack");
+            PV2.Content = Joueur2.PV;
+
 
         }
 
         private void J2_AttPhys_Btn(object sender, RoutedEventArgs e)
         {
             PlayerAttack(Joueur2, Joueur1, "Physical Attack");
-
+            PV1.Content = Joueur1.PV;
         }
 
         private void J2_AttInt_Btn(object sender, RoutedEventArgs e)
         {
             PlayerAttack(Joueur2, Joueur1, "Intellectual Attack");
-
+            PV1.Content = Joueur1.PV;
         }
 
         private void PlayerAttack(Character player1, Character player2, string attacjType)
@@ -71,7 +81,9 @@ namespace Hackathon2
             attackChoice.defenderPlayer = player2;
             attackChoice.playerAttackType = attacjType;
             Fight.SetUserAttackChoice(attackChoice);
-            AQuiLeTour.Content = "J1 = " + player1.PV + " J2 = " + player2.PV;
+            
+
+            //AQuiLeTour.Content = "J1 = " + player1.PV + " J2 = " + player2.PV;
             if (Fight.Winner != null)
             {
                 DisplayWinner();
